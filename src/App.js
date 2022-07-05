eaf21856d8c009cd31816a7584f068b2bab3e4b7
raamprasaadh <ramprasadhsurya@gmail.com>
2022-07-05 20:01:26 +0530

    adds a bar chart

--- src/App.js
+++ src/App.js
@@ -10,16 +10,27 @@ function App() {
       { subject: "Fish", count: 130 },
       { subject: "Bunnies", count: 185 },
     ];
+
     d3.select("#pgraph")
       .selectAll("p")
       .data(dataSet)
       .enter()
       .append("p")
       .text((dt) => dt.subject + " " + dt.count);
+
+      var getMax=(dataSet)=>{
+        var max=0;
+        dataSet.map(dt=>(dt.count>max)?max=dt.count:null)
+        return max;
+      }
+      d3.selectAll("#barChart").selectAll("div").data(dataSet).enter().append("div").classed("bar", true).style('height',`${getMax(dataSet)}px`)
+      d3.select("#barChart").selectAll(".bar")
+.transition().duration(1000).style("height",bar=>`${bar.count}px`).style("width","80px").style("margin-right","10px").delay(300);
   }, []);
   return (
     <div className="App">
       <div id="pgraph"></div>
+      <div id="barChart"></div>
     </div>
   );
 }
