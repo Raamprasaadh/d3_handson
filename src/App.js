55361ea80446fc6ed3eda63dee7c194dfc0d005e
raamprasaadh <ramprasadhsurya@gmail.com>
2022-07-06 08:47:28 +0530

    practice dom manipulation & bar chart

--- src/App.js
+++ src/App.js
@@ -18,19 +18,85 @@ function App() {
       .append("p")
       .text((dt) => dt.subject + " " + dt.count);
 
-      var getMax=(dataSet)=>{
-        var max=0;
-        dataSet.map(dt=>(dt.count>max)?max=dt.count:null)
-        return max;
-      }
-      d3.selectAll("#barChart").selectAll("div").data(dataSet).enter().append("div").classed("bar", true).style('height',`${getMax(dataSet)}px`)
-      d3.select("#barChart").selectAll(".bar")
-.transition().duration(1000).style("height",bar=>`${bar.count}px`).style("width","80px").style("margin-right","10px").delay(300);
+    d3.select("#pgraph1")
+      .selectAll("p")
+      .data(dataSet)
+      .enter()
+      .append("p")
+      .text((dt) => `${dt.subject} ${dt.count}`);
+
+    var getMax = (dataSet) => {
+      var max = 0;
+      dataSet.map((dt) => (dt.count > max ? (max = dt.count) : null));
+      return max;
+    };
+    var getMax1 = (dataSet) => {
+      var max = 0;
+      dataSet.map((data) => (data.count > max ? (max = data.count) : null));
+      return max;
+    };
+    var getMax2 = (dataSet) => {
+      var max = 0;
+      dataSet.map((data) => (data.count > max ? (max = data.count) : null));
+      return max;
+    };
+    d3.selectAll("#barChart")
+      .selectAll("div")
+      .data(dataSet)
+      .enter()
+      .append("div")
+      .classed("bar", true)
+      .style("height", `${getMax(dataSet)}px`);
+    d3.select("#barChart")
+      .selectAll(".bar")
+      .transition()
+      .duration(1000)
+      .style("height", (bar) => `${bar.count}px`)
+      .style("width", "80px")
+      .style("margin-right", "10px")
+      .delay(300);
+
+    //barChart1
+    d3.select("#barChart1")
+      .selectAll("div")
+      .data(dataSet)
+      .enter()
+      .append("div")
+      .classed("bar", true)
+      .style("height", `${getMax1(dataSet)}px`);
+    d3.select("#barChart1")
+      .selectAll(".bar")
+      .transition()
+      .duration(1000)
+      .style("height", (bar) => `${bar.count}px`)
+      .style("width", "80px")
+      .style("margin-right", "10px")
+      .delay(300);
+
+    //barchart 2;
+    d3.selectAll("#barChart2")
+      .selectAll("div")
+      .data(dataSet)
+      .enter()
+      .append("div")
+      .classed("bar", true)
+      .style("height", `${getMax2(dataSet)}px`);
+    d3.select("#barChart2")
+      .selectAll(".bar")
+      .transition()
+      .duration(1000)
+      .style("height", (bar) => `${bar.count}px`)
+      .style("width", "80px")
+      .style("margin-right", "10px")
+      .delay(300);
   }, []);
   return (
     <div className="App">
       <div id="pgraph"></div>
+      <div id="pgraph1"></div>
       <div id="barChart"></div>
+      <div id="barChart1"></div>
+      <div id="barChart2"></div>
     </div>
   );
 }
